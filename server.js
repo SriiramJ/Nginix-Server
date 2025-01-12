69672532d24325c3e7f0ebefbdb449a760ef51d9
Sriram <sriram2005j@gmail.com>
2025-01-12 12:21:40 +0530

    add filepath code in server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : "req.url"
+  );
 
-server.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
-    
-})
+  const extName = String(path.extname(filePath)).toLowerCase();
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
