02d0233f01ab7c536352e31ac2db20eb77ca85be
Sriram <sriram2005j@gmail.com>
2025-01-12 13:59:59 +0530

    debugging done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   const filePath = path.join(
     __dirname,
-    req.url === "/" ? "index.html" : "req.url"
+    req.url === "/" ? "index.html" : req.url
   );
 
   const extName = String(path.extname(filePath)).toLowerCase();
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
     ".html": "text/html",
     ".css": "text/css",
     ".js": "text/javascript",
-    ".png": "text/png",
+    ".png": "image/png",
   };
 
   const contentType = mimeTypes[extName] || "application/octet-stream";
@@ -24,16 +24,16 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code === "ENOENT") {
-        res.writeHead(404, { "content-Type": "text/html" });
+        res.writeHead(404, { "Content-Type": "text/html" });
         res.end("404: File Not Found");
       }
     } else {
-      res.writeHead(200, { "content-Type": contentType });
+      res.writeHead(200, { "Content-Type": contentType });
       res.end(content, "utf-8");
     }
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
